68b49dadd1ba70286be3fda61d84902e0cac0775
EnzeChen0314 <ec3576@columbia.edu>
2022-02-23 13:38:29 -0500

    Add files via upload

---
+++ lab2/lab2.c
@@ -0,0 +1,165 @@
+/*
+ *
+ * CSEE 4840 Lab 2 for 2019
+ *
+ * Name/UNI: Enze Chen/ Youfeng Chen
+ */
+#include "fbputchar.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include "usbkeyboard.h"
+#include <pthread.h>
+
+/* Update SERVER_HOST to be the IP address of
+ * the chat server you are connecting to
+ */
+/* arthur.cs.columbia.edu */
+#define SERVER_HOST "128.59.19.114"
+#define SERVER_PORT 42000
+
+#define BUFFER_SIZE 128
+
+#define MAX_COL 64
+#define MAX_ROW_R 20
+#define MAX_ROW_S 23
+
+/*
+ * References:
+ *
+ * http://beej.us/guide/bgnet/output/html/singlepage/bgnet.html
+ * http://www.thegeekstuff.com/2011/12/c-socket-programming/
+ * 
+ */
+
+int sockfd; /* Socket file descriptor */
+
+struct libusb_device_handle *keyboard;
+uint8_t endpoint_address;
+
+pthread_t network_thread;
+void *network_thread_f(void *);
+
+void initial();
+void memRclear();
+void memSclear();
+
+int main()
+{
+  int err, col;
+
+  struct sockaddr_in serv_addr;
+
+  struct usb_keyboard_packet packet;
+  int transferred;
+  char keystate[12];
+
+
+  // initial the screen display
+  initial();
+
+  /* Open the keyboard */
+  if ( (keyboard = openkeyboard(&endpoint_address)) == NULL ) {
+    fprintf(stderr, "Did not find a keyboard\n");
+    exit(1);
+  }
+    
+  /* Create a TCP communications socket */
+  if ( (sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0 ) {
+    fprintf(stderr, "Error: Could not create socket\n");
+    exit(1);
+  }
+
+  /* Get the server address */
+  memset(&serv_addr, 0, sizeof(serv_addr));
+  serv_addr.sin_family = AF_INET;
+  serv_addr.sin_port = htons(SERVER_PORT);
+  if ( inet_pton(AF_INET, SERVER_HOST, &serv_addr.sin_addr) <= 0) {
+    fprintf(stderr, "Error: Could not convert host IP \"%s\"\n", SERVER_HOST);
+    exit(1);
+  }
+
+  /* Connect the socket to the server */
+  if ( connect(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
+    fprintf(stderr, "Error: connect() failed.  Is the server running?\n");
+    exit(1);
+  }
+
+  /* Start the network thread */
+  pthread_create(&network_thread, NULL, network_thread_f, NULL);
+
+  /* Look for and handle keypresses */
+  for (;;) {
+    libusb_interrupt_transfer(keyboard, endpoint_address,
+			      (unsigned char *) &packet, sizeof(packet),
+			      &transferred, 0);
+    if (transferred == sizeof(packet)) {
+      sprintf(keystate, "%02x %02x %02x", packet.modifiers, packet.keycode[0],
+	      packet.keycode[1]);
+      printf("%s\n", keystate);
+      fbputs(keystate, 6, 0);
+      if (packet.keycode[0] == 0x29) { /* ESC pressed? */
+	break;
+      }
+    }
+  }
+
+  /* Terminate the network thread */
+  pthread_cancel(network_thread);
+
+  /* Wait for the network thread to finish */
+  pthread_join(network_thread, NULL);
+
+  return 0;
+}
+
+void *network_thread_f(void *ignored)
+{
+  char recvBuf[BUFFER_SIZE];
+  int n;
+  /* Receive data */
+  while ( (n = read(sockfd, &recvBuf, BUFFER_SIZE - 1)) > 0 ) {
+    recvBuf[n] = '\0';
+    printf("%s", recvBuf);
+    fbputs(recvBuf, 8, 0);
+  }
+
+  return NULL;
+}
+
+void initial()
+{
+  if ((err = fbopen()) != 0) {
+    fprintf(stderr, "Error: Could not open framebuffer: %d\n", err);
+    exit(1);
+  }
+
+  /* Draw rows of asterisks across the top and bottom of the screen */
+  // Draw the horizontal line that split into receive region and send region
+  for (col = 0 ; col < MAX_COL ; col++) {
+    fbputchar('*', 0, col);
+    fbputchar('-', MAX_ROW_R, col);
+    fbputchar('*', MAX_ROW_S, col);
+  }
+
+  // Screen Clean
+  memRclear();
+  memSclear();
+}
+
+void memRclear()
+{
+  for (col = 0 ; col < MAX_COL ; col++) {
+    for (row = 0 ; row < MAX_ROW_R ; row++) fbputchar(' ', row, col);
+  }
+}
+
+void memSclear()
+{
+  for (col = 0 ; col < MAX_COL ; col++) {
+    for (row = MAX_ROW_R+1 ; row < MAX_ROW_S ; row++) fbputchar(' ', row, col);
+  }
+}
